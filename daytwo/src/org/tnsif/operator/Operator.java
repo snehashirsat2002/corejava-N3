39f2556a42867df99ddef0d8670ff79f61dd4d8a
SNEHA SHIRSAT <SNEHA SHIRSAT@LAPTOP-O14ERIQO>
2023-02-06 10:48:50 +0530

    Second Commit

---
+++ daytwo/src/org/tnsif/operator/Operator.java
@@ -0,0 +1,30 @@
+package org.tnsif.operator;
+
+public class Operator {
+ public static void main(String[]args) {
+	 byte a=127;
+	 int b=128;
+	 float c=12/5;
+	 float e=12/2.5f;
+	 double f=12/5;
+	 double d=12d/5d;
+	 //character
+	 char z='s';
+	 //ASCII representation
+	 int x='c';
+	 //string
+	 String name ="sneha";
+	 //boolean type
+	 boolean y=true;
+	 
+	 System.out.println("The value of a is:"+a);
+	 System.out.println("The value of b is:"+b);
+	 System.out.println("The value of c is:"+c);
+	 System.out.println("The value of d is:"+d);
+	 System.out.println("The value of e is:"+e);
+	 System.out.println("The value of f is:"+f);
+	 System.out.println("The value of x is:"+x);
+	 System.out.println("The value of z is:"+z);
+	 System.out.println("The value of string is:"+name);
+ }
+}
